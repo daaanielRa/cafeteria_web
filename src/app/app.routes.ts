2eecd5b8aa5ef665b804e7ce22b2cf9fb98d0da9
daaanielRa <daaaniel2211ra@gmail.com>
2025-02-19 19:57:24 -0500

    Avances 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { FregistroComponent } from './pages/fregistro/fregistro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginComponent
+    },
+    {
+        path: "registros",
+        component: FregistroComponent 
+    }
+];
---
+++ src/app/pages/fregistro/fregistro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fregistro',
+  imports: [],
+  templateUrl: './fregistro.component.html',
+  styleUrl: './fregistro.component.css'
+})
+export class FregistroComponent {
+
+}
