0ed4118b27851fc33a68fde43f87782784374b14
daaanielRa <daaaniel2211ra@gmail.com>
2025-02-28 18:59:50 -0500

    Vista general

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { FregistroComponent } from './pages/fregistro/fregistro.component';
+import { InteradminComponent } from './pages/interadmin/interadmin.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "registros",
         component: FregistroComponent 
+    },
+    {
+        path: "interadmin",
+        component: InteradminComponent
     }
 ];
---
+++ src/app/pages/interadmin/interadmin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interadmin',
+  imports: [],
+  templateUrl: './interadmin.component.html',
+  styleUrl: './interadmin.component.css'
+})
+export class InteradminComponent {
+
+}
