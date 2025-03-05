3f71bcb74ce7e46765b3839cf2d4d93ba034e138
daaanielRa <daaaniel2211ra@gmail.com>
2025-03-05 18:30:26 -0500

    Avances

---
+++ src/app/pages/historiaventas/historiaventas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historiaventas',
+  imports: [],
+  templateUrl: './historiaventas.component.html',
+  styleUrl: './historiaventas.component.css'
+})
+export class HistoriaventasComponent {
+
+}
