985d667016d69b22b43138a24a596413aeb1cecc
daaanielRa <daaaniel2211ra@gmail.com>
2025-03-28 18:38:03 -0500

    Avances

---
+++ src/utils/types/empleados.ts
@@ -0,0 +1,8 @@
+export interface Empleados {
+    nombre: string
+    correo: string
+    cargo: string
+    horario: Date
+    clave: string
+    tipoUsuario: string
+}
--- src/views/empleadosView.vue
+++ src/views/empleadosView.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
 import PageBase from '@/components/layout/pageBase.vue'
 import TablaEmpleados from '@/components/tables/tablaEmpleados.vue'
+import { FirebaseService } from '@/services/firebaseService'
+import { collection, addDoc } from 'firebase/firestore'
+import { ref } from 'vue'
+
+const formEmpleado = ref({
+  nombre: "",
+  correo: "",
+  cargo: "",
+  horario: new Date(),
+  clave: "",
+  tipoUsuario: "",
+})
+
+async function anadirEmpleado(){
+  try {
+    const col=collection(FirebaseService.db,"empleado") 
+  await addDoc(col, formEmpleado.value)
+    alert("empleado añadido")
+  } catch (error) {
+    alert("error al añadir")
+    console.error(error);
+    
+  }
+}
+
 </script>
 
 <template>
@@ -9,7 +34,7 @@ import TablaEmpleados from '@/components/tables/tablaEmpleados.vue'
       <div class="content">
         <p>Gestión de Empleados</p>
 
-        <form>
+        <form @submit.prevent="anadirEmpleado">
           <div>
             <label for="Nombre">Nombre:</label>
             <input type="text" id="Nombre" placeholder="Nombre" class="font-poppins" required />
@@ -18,7 +43,7 @@ import TablaEmpleados from '@/components/tables/tablaEmpleados.vue'
           <div>
             <label for="correo">Correo Electrónico:</label>
             <input
-              type="email"
+              type="email" v-model="formEmpleado.correo"
               id="correo"
               placeholder="Correo Electrónico"
               class="font-poppins"
@@ -28,21 +53,21 @@ import TablaEmpleados from '@/components/tables/tablaEmpleados.vue'
 
           <div>
             <label for="cargo">Cargo:</label>
-            <input type="text" id="cargo" placeholder="Cargo" class="font-poppins" required />
+            <input type="text" v-model="formEmpleado.cargo" id="cargo" placeholder="Cargo" class="font-poppins" required />
           </div>
 
           <div>
             <label for="horario">Horario:</label>
-            <input type="time" id="horario" required />
+            <input type="time" v-model="formEmpleado.horario" id="horario" required />
           </div>
 
           <div>
             <label for="clave">Clave:</label>
-            <input type="password" id="clave" placeholder="Clave" class="font-poppins" required />
+            <input type="password" v-model="formEmpleado.clave" id="clave" placeholder="Clave" class="font-poppins" required />
           </div>
 
           <label for="usuario">Tipo de usuario:</label>
-          <select id="usuario" name="usuario" required>
+          <select v-model="formEmpleado.tipoUsuario" id="usuario" name="usuario" required>
             <option value="" selected disabled></option>
             <option value="empleado">Empleado</option>
             <option value="administrador">Administrador</option>
